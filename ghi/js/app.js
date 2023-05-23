2aeaf305db442b5300b3669c8d86baafbb22ab28
Ahad <ahadali153@gmail.com>
2023-05-23 17:59:10 -0400

    added date and location name

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,11 +1,18 @@
-function createCard(name, description, pictureUrl) {
+function createCard(name, description, pictureUrl, starts, ends, location) {
     return `
-        <div class="card shadow p-3 mb-5 bg-body-tertiary rounded mtb-md-5">
-            <img src="${pictureUrl}" class="card-img-top">
+    <div class="col-4">
+        <div class="card text-center shadow my-lg-3">
             <div class="card-body">
-            <h5 class="card-title">${name}</h5>
-            <p class="card-text">${description}</p>
+                <img src="${pictureUrl}" class="card-img-top">
+                <h5 class="card-title">${name}</h5>
+                <h6 class="card-subtitle text-muted">${location}</h6>
+                <p class="card-text">${description}</p>
+            </div>
+            <div class="card-footer">
+                ${starts} - ${ends}
+            </div>
         </div>
+    </div>
     `;
   }
 
@@ -20,7 +27,6 @@ try {
     // Figure out what to do when the response is bad
     } else {
     const data = await response.json();
-    let count = 0
     for (let conference of data.conferences) {
         const detailUrl = `http://localhost:8000${conference.href}`;
         const detailResponse = await fetch(detailUrl);
@@ -29,14 +35,12 @@ try {
         const title = details.conference.name;
         const description = details.conference.description;
         const pictureUrl = details.conference.location.picture_url;
-        const html = createCard(title, description, pictureUrl);
-        const column = document.querySelectorAll('.col');
-        column[count].innerHTML += html;
-        if (count !== 2) {
-            count ++
-        } else {
-            count = 0
-        }
+        const starts = new Date(details.conference.starts).toLocaleDateString();
+        const ends = new Date(details.conference.ends).toLocaleDateString();
+        const location = details.conference.location.name;
+        const html = createCard(title, description, pictureUrl, starts, ends, location);
+        const column = document.querySelector('.row');
+        column.innerHTML += html;
         }
     }
 
