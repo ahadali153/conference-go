81c6d4f5799aa354bfad3e810d6a7685fe55803b
Ahad <ahadali153@gmail.com>
2023-05-23 16:28:30 -0400

    looping conferences

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,34 +1,49 @@
+function createCard(name, description, pictureUrl) {
+    return `
+        <div class="card shadow p-3 mb-5 bg-body-tertiary rounded mtb-md-5">
+            <img src="${pictureUrl}" class="card-img-top">
+            <div class="card-body">
+            <h5 class="card-title">${name}</h5>
+            <p class="card-text">${description}</p>
+        </div>
+    `;
+  }
+
 window.addEventListener('DOMContentLoaded', async () => {
 
-    const url = 'http://localhost:8000/api/conferences/';
+const url = 'http://localhost:8000/api/conferences/';
 
-    try {
-      const response = await fetch(url);
+try {
+    const response = await fetch(url);
 
-      if (!response.ok) {
-        // Figure out what to do when the response is bad
-      } else {
-        const data = await response.json();
-
-        const conference = data.conferences[0];
-        const nameTag = document.querySelector('.card-title');
-        nameTag.innerHTML = conference.name;
-
+    if (!response.ok) {
+    // Figure out what to do when the response is bad
+    } else {
+    const data = await response.json();
+    let count = 0
+    for (let conference of data.conferences) {
         const detailUrl = `http://localhost:8000${conference.href}`;
         const detailResponse = await fetch(detailUrl);
-
         if (detailResponse.ok) {
-          const details = await detailResponse.json();
-          const description = details.conference.description;
-          const descriptionTag = document.querySelector('.card-text');
-          descriptionTag.innerHTML = description;
-          const imageTag = document.querySelector('.card-img-top')
-          imageTag.src = details.conference.location.picture_url;
+        const details = await detailResponse.json();
+        const title = details.conference.name;
+        const description = details.conference.description;
+        const pictureUrl = details.conference.location.picture_url;
+        const html = createCard(title, description, pictureUrl);
+        const column = document.querySelectorAll('.col');
+        column[count].innerHTML += html;
+        if (count !== 2) {
+            count ++
+        } else {
+            count = 0
         }
-
-      }
-    } catch (e) {
-      // Figure out what to do if an error is raised
+        }
     }
 
-  });
+    }
+} catch (e) {
+
+    // Figure out what to do if an error is raised
+}
+
+});
